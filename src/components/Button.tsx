570d0a721ca1969c29888af9cb085b93013da1dc
priyanshuyxdev <priyanshuyxdev@gmail.com>
2025-06-28 23:49:58 +0530

    Navbar

---
+++ src/components/Button.tsx
@@ -0,0 +1,30 @@
+import { HTMLAttributes } from "react";
+import { cva } from "class-variance-authority";
+
+const classes = cva("border rounded-full h-12 px-6 font-medium", {
+    variants: {
+        variant: {
+            primary: "bg-lime-400 text-neutral-950 border-ime-400",
+
+            secondary: "border-white text-white bg-transparent",
+        },
+    },
+});
+
+export default function Button(
+    props: {
+        variant: "primary" | "secondary";
+    } & HTMLAttributes<HTMLButtonElement>
+) {
+
+    const {variant,className, ...otherProps} = props;
+    return (
+        <button
+            className={classes({
+                variant,
+                className,
+            })}
+            {...otherProps}
+        ></button>
+    );
+}
