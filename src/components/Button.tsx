102cbb6f40e02ebe29188a4b3ad6d7aa5b05aa1b
priyanshuyxdev <priyanshuyxdev@gmail.com>
2025-06-30 15:43:06 +0530

    hero-sectio mobile/tablet view

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,24 +4,29 @@ import { cva } from "class-variance-authority";
 const classes = cva("border rounded-full h-12 px-6 font-medium", {
     variants: {
         variant: {
-            primary: "bg-lime-400 text-neutral-950 border-ime-400",
+            primary: "bg-lime-400 text-neutral-950 border-lime-400",
 
             secondary: "border-white text-white bg-transparent",
         },
+        size:{
+            sm:"h-10"
+        }
     },
 });
 
 export default function Button(
     props: {
         variant: "primary" | "secondary";
+        size?:'sm';
     } & HTMLAttributes<HTMLButtonElement>
 ) {
 
-    const {variant,className, ...otherProps} = props;
+    const {variant,className,size, ...otherProps} = props;
     return (
         <button
             className={classes({
                 variant,
+                size,
                 className,
             })}
             {...otherProps}
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,29 @@
+import Button from "@/components/Button";
+
 export default function Hero() {
-    return <div>Hero</div>;
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center">
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400  rounded-full font-semibold text-neutral-900 ">
+                        ✨$7.5m seed round raised
+                    </div>
+                </div>
+                <h1 className="text-6xl md:text-7xl mt-6 text-center font-medium">
+                    Impactful design, created effortlessly
+                </h1>
+                <p className="text-xl mt-8 text-white/50 text-center max-w-2xl mx-auto">
+                    Design tools shouldn't slow you down. Layers combines
+                    powerful features with an intuitive interface that keeps you
+                    in your creative flow.
+                </p>
+
+                <form className="flex border border-white/15 rounded-full mt-8 p-2 max-w-lg mx-auto">
+                    <input type="email" placeholder="Enter your email" className="bg-transparent outline-none px-4 w-40  flex-1"/>
+                  
+                    <Button className="whitespace-nowrap " variant="primary" size="sm">Sign up</Button>
+                </form>
+            </div>
+        </section>
+    );
 }
